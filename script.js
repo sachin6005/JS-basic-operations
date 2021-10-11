2cdd3a501af299954d2bea7921dcb544a251699d
sachin kumar <merugune.sachin123@gmail.com>
2021-10-11 22:04:58 +0530

    basics of javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+console.clear()
+var num1 = 10
+var num2 = 20
+var num3 = 15.25
+var num4 = '22'
+
+console.log(typeof(num2))
+console.log(typeof(num3))
+console.log(typeof(num4))
+console.log(typeof(num5))
+
+sum = num1+num2+num3
+console.log(sum)
+
+sub = num2-num1
+console.log(sub)
+
+mul = num1 * num2 
+console.log(mul)
+
+div = num3/num1
+console.log(div)
